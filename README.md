0d41d4bfb5a739ce957ee04a44a4d6c4ec31c0a9
SaidBoy <alkhudrimsaid733@gmail.com>
2023-10-21 14:05:19 +0700

    embed file, gambar, multiple files

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 1. golang embed
     - fitur golang embed ada di golang >= 1.16 , kurang dari itu tidak ada fitur ini
 
-2. 
+2. golang embed intinya 
+    - adalah memungkinkan kita untuk dapat memasukkan sebuah file (apapun itu text,foto, html dll) kedalam program golang kita.
+
+    - //go:embed <namafile> adalah perintah untuk menggunakan fitur golang embed ini. untuk namafile harus sama persis.
+
+    - variabel yang akan digunakan untuk embed tidak boleh berada didalam function.
+
+3. Image
+    - gambar ketika di embed maka dia akan menghasilkan []byte. dari byte tersebut kita bisa mengubahnya kembali ke file gambar (copy) dan memberi nama berbeda.
+
+4. Multiple File
+    - jika anda ingin membaca banyak file sekaligus gunakan tipe embed.FS untuk dapat membuka satu persatu.
---
+++ embed_test.go
@@ -0,0 +1,51 @@
+package golangembed
+
+import (
+	"embed" // jika lebih gunakan yang ini.
+	_ "embed" // digunakan jika anda hanya memanggil //go:embed <filename> saja
+	"fmt"
+	"io/fs"
+	"os"
+	"testing"
+)
+
+//go:embed hello.txt
+var Hello string // tipe datanya harus menyesuaikan dengan isi filenya.
+
+//go:embed image/said.jpeg
+var Image []byte // karena gambar bertipe []byte
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+var files embed.FS
+
+func TestHello(t *testing.T) {
+	fmt.Println(Hello)
+}
+
+func TestImage(t *testing.T) {
+	fmt.Println(Image)
+}
+
+func TestCopyImage(t *testing.T) {
+	err := os.WriteFile("image/said.copy.jpeg", Image, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMultipleFiles(t *testing.T) {
+	a, err := files.ReadFile("files/a.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := files.ReadFile("files/b.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(a))
+	fmt.Println(string(b))
+}
+
